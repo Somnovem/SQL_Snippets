2438ed7f2f6526ba5942f947bb04a79fcd58d4fc
Somnovem <artem.zhmura606@gmail.com>
2022-12-24 17:16:13 +0200

    24.12 update

--- SQLQuery1-Academy.sql
+++ SQLQuery1-Academy.sql
@@ -1,9 +1,3 @@
-use [master]
-go
-
-create database [Academy]
-go
-
 use [Academy]
 go
 
@@ -32,15 +26,20 @@ constraint CK_Departments_Financing check([Financing] >=0),
 create table [Faculties](
 [Id] int not null identity(1,1),
 [Name] nvarchar(100) not null unique,
+[Dean] nvarchar(max) not null,
 
 constraint PK_Faculties_Id primary key([Id]),
 constraint CK_Faculties_Name check([Name] <>''),
+constraint CK_Faculties_Dean check([Dean]<>'')
 );
 
 create table [Teachers](
 [Id] int not null identity(1,1),
 [EmploymentDate] date not null,
+[IsAssistant] bit not null default(0),
+[IsProfessor] bit not null default(0),
 [Name] nvarchar(max) not null,
+[Position] nvarchar(max) not null,
 [Surname] nvarchar(max) not null,
 [Premium] money not null default(0),
 [Salary] money not null,
@@ -48,7 +47,8 @@ create table [Teachers](
 constraint PK_Teachers_Id primary key([Id]),
 constraint CK_Teachers_Name check([Name] <>''),
 constraint CK_Teachers_Surname check([Surname] <>''),
-constraint CK_Teachers_EmploymentDate check([EmploymentDate] >'01.01.1990'),
+constraint CK_Teachers_Position check([Position] <>''),
+constraint CK_Teachers_EmploymentDate check([EmploymentDate] >='01.01.1990'),
 constraint CK_Teachers_Premium check([Premium] >=0),
 constraint CK_Teachers_Salary check([Salary] >0),
 );
